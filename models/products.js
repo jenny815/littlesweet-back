9504e54052e3a3af968df9dcbbbc824b0949ef5f
jenny815 <80014561+jenny815@users.noreply.github.com>
2021-08-23 15:10:01 +0800

    Add files via upload

---
+++ models/products.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, '品名不能為空'],
+      minlength: [1, '品名不能為空']
+    },
+    price: {
+      type: Number,
+      min: [0, '價格格式不正確'],
+      required: [true, '價格不能為空']
+    },
+    description: {
+      type: String
+    },
+    image: {
+      type: String
+    },
+    sell: {
+      type: Boolean,
+      default: false
+    },
+    category: {
+      type: String,
+      enum: ['蛋糕', '肉桂捲', '巧克力', '餅乾']
+    }
+  },
+  { versionKey: false }
+)
+
+export default mongoose.model('products', productSchema)
